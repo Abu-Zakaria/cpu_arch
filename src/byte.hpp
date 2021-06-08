dbf4c9774240f4fadb19364ec43fdfeb6b812443
Jakaria Blaine <jakariablaine120@gmail.com>
2021-06-08 06:01:11 +0600

    Add byte header file

---
+++ src/byte.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "m.hpp"
+
+class Byte
+{
+    private:
+        bool    i0, // inputs
+                i1,
+                i2,
+                i3,
+                i4,
+                i5,
+                i6,
+                i7;
+
+        bool    o0, // ouputs
+                o1,
+                o2,
+                o3,
+                o4,
+                o5,
+                o6,
+                o7;
+        M   m0, // 1 bit memory cells
+            m1,
+            m2,
+            m3,
+            m4,
+            m5,
+            m6,
+            m7;
+
+        bool s; // save state
+
+        const char boolToChar(bool value);
+
+    public:
+        // @param const char* inputs - example "00101100"
+        void set(const char* inputs);
+
+        void setS(bool s);
+
+        const char* get();
+
+        void update();
+}
